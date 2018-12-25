2c385781e3db17d061a17680b5a98b5e6508765d
Anas Qadrei <anasqadrei@gmail.com>
2018-12-25 17:43:10 +1100

    footer

--- components/footnote.js
+++ components/footnote.js
@@ -1,58 +1,59 @@
 import Head from './head'
 import Link from 'next/link'
 
-const links = [
-  { href: '/about', label: 'About' },
-  { href: '/blog', label: 'blog' },
-  { href: '/terms', label: 'terms' },
-  { href: '/privacy', label: 'privacy' },
-  { href: '/copyright', label: 'copyright' },
-  { href: '/contact', label: 'contact us' },
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
-
 const Footnote = () => (
-  <nav>
-    <ul>
-      {links.map(
-        ({ key, href, label }) => (
-          <li key={key}>
-            <Link href={href}>
-              <a>{label}</a>
-            </Link>
-          </li>
-        )
-      )}
-    </ul>
-
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system,BlinkMacSystemFont,Avenir Next,Avenir,Helvetica,sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
-  </nav>
+  <footer>
+    <div>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link href="/terms">
+        <a>Terms</a>
+      </Link>
+      <Link href="/privacy">
+        <a>Privacy</a>
+      </Link>
+      <Link href="/cookies">
+        <a>Cookies</a>
+      </Link>
+      <Link href="/copyright">
+        <a>Copyright</a>
+      </Link>
+      <Link href="/contact">
+        <a>Contact Us</a>
+      </Link>
+    </div>
+    <div>
+      Follow us on social media
+      <Link href="/">
+        <a>Facebook</a>
+      </Link>
+      <Link href="/">
+        <a>Instagram</a>
+      </Link>
+      <Link href="/">
+        <a>Twitter</a>
+      </Link>
+      <Link href="/">
+        <a>Google+</a>
+      </Link>
+      <Link href="/">
+        <a>YouTube</a>
+      </Link>
+    </div>
+    <div>
+      Download our apps
+      <Link href="/">
+        <a>App Store</a>
+      </Link>
+      <Link href="/">
+        <a>Google Play</a>
+      </Link>
+    </div>
+  </footer>
 )
 
 export default Footnote
