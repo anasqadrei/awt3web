3b5ebf27f2ad7eb0a703197001c8218629edd312
Anas Qadrei <anasqadrei@gmail.com>
2019-11-25 12:48:42 +1100

    artist page and comp

--- components/artist.comp.js
+++ components/artist.comp.js
@@ -1,8 +1,44 @@
+import Link from 'next/link'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import Raven from 'raven-js'
 import Head from './head'
+import Comment from './comment.comp'
+import CommentsList from './commentsList.comp'
 import ErrorMessage from './errorMessage'
+
+const recentlyAddedSongs = []
+for (let i = 0; i < 10; i++) {
+  recentlyAddedSongs.push(
+    <Link key={i} as="/song/1/slug" href={`/song?id=1`}>
+      <a>song title </a>
+    </Link>
+  )
+}
+
+const popularSongs = []
+for (let i = 0; i < 10; i++) {
+  popularSongs.push(
+    <Link key={i} as="/song/1/slug" href={`/song?id=1`}>
+      <a>song title </a>
+    </Link>
+  )
+}
+
+const allSongs = []
+for (let i = 0; i < 20; i++) {
+  allSongs.push(
+    <div key={i}>
+      <img src="https://via.placeholder.com/30?text=song+image"/>
+      <Link as="/song/1/slug" href={`/song?id=1`}>
+        <a>song title</a>
+      </Link>
+      <img src="https://via.placeholder.com/30?text=duration"/> 3:25
+      <img src="https://via.placeholder.com/30?text=playsCount"/> 2,323
+      <img src="https://via.placeholder.com/30?text=More+Actions"/>
+    </div>
+  )
+}
 
 const artistQuery = gql`
   query getArtist ($id: ID!) {
@@ -12,6 +48,8 @@ const artistQuery = gql`
       slug
       imageUrl
       likes
+      shares
+      comments
       songs
       songUsersPlayed
       songPlays
@@ -32,10 +70,16 @@ function ArtistData({ data: { loading, error, getArtist }, ownProps }) {
     return (<div>Artist doesn't exist (design this)</div>)
   } else if (getArtist) {
     if (ownProps.fixSlug) {
-      const re = new RegExp ('^' + ownProps.router.pathname + '/' + ownProps.router.query.id + '/' + getArtist.slug + '([?].*|[#].*|/)?$')
-      if (!decodeURIComponent(ownProps.router.asPath).match(re)) {
-        const href = ownProps.router.pathname + '?id=' + ownProps.router.query.id + '&slug=' + getArtist.slug
-        const as = ownProps.router.pathname + '/' + ownProps.router.query.id + '/' + getArtist.slug
+      // const re = new RegExp ('^' + ownProps.router.pathname + '/' + ownProps.router.query.id + '/' + getArtist.slug + '([?].*|[#].*|/)?$')
+      // if (!decodeURIComponent(ownProps.router.asPath).match(re)) {
+      //   const href = ownProps.router.pathname + '?id=' + ownProps.router.query.id + '&slug=' + getArtist.slug
+      //   const as = ownProps.router.pathname + '/' + ownProps.router.query.id + '/' + getArtist.slug
+      //   ownProps.router.replace(href, as)
+      // }
+      const regExp = new RegExp (`^${ ownProps.router.pathname }/${ ownProps.router.query.id }/${ getArtist.slug }([?].*|[#].*|/)?$`)
+      if (!decodeURIComponent(ownProps.router.asPath).match(regExp)) {
+        const href = `${ ownProps.router.pathname }?id=${ ownProps.router.query.id }&slug=${ getArtist.slug }`
+        const as = `${ ownProps.router.pathname }/${ ownProps.router.query.id }/${ getArtist.slug }`
         ownProps.router.replace(href, as)
       }
     }
@@ -43,16 +87,49 @@ function ArtistData({ data: { loading, error, getArtist }, ownProps }) {
       <div>
         <Head title={ getArtist.name } description={ getArtist.name } url={ getArtist.url } ogImage={ getArtist.imageUrl } />
         <div>
+          <img src={ getArtist.imageUrl?getArtist.imageUrl:`https://via.placeholder.com/100?text=no+photo?` }/>
           <h1 className="title">{ getArtist.name }</h1>
-          <img src={ getArtist.imageUrl } />
-          <p className="description">عدد المعجبين: { getArtist.likes }</p>
-          <p className="description">عدد الاغاني: { getArtist.songs }</p>
-          <p className="description">عدد المستمعين: { getArtist.songUsersPlayed }</p>
-          <p className="description">عدد سماع الاغاني: { getArtist.songPlays }</p>
-          <p className="description">عدد الاغاني المفضلة: { getArtist.songLikes }</p>
-          <p className="description">الصور: { getArtist.songImages }</p>
-          <p className="description">الرابط: <span dir="ltr">{ getArtist.url }</span></p>
+          <Link href=""><a>Like</a></Link>
+          { getArtist.likes?`${ getArtist.likes } liked them`:`be the first to like? or empty?` }
         </div>
+
+        <div>
+          New Songs
+          {recentlyAddedSongs}
+        </div>
+
+        <div>
+          Share
+          { getArtist.shares?`${ getArtist.shares } shared this`:`be the first to share` }
+          <Link href="/"><a>Facebook</a></Link>
+          <Link href="/"><a>Twitter</a></Link>
+          <Link href="/"><a>Google+</a></Link>
+          <span dir="ltr"><input value={ getArtist.url } readOnly/></span>
+        </div>
+
+        <div>
+          <img src="https://via.placeholder.com/728x90?text=728x90+Leaderboard+Ad+but+will+be+responsive"/>
+        </div>
+
+        <div>
+          Popular songs
+          {popularSongs}
+        </div>
+
+        <div>
+          Data?
+          { getArtist.songUsersPlayed?`${ getArtist.songUsersPlayed } listers`:`` }
+          Songs played: { getArtist.songPlays?getArtist.songPlays:0 }
+        </div>
+
+        <div>
+          All songs (sort: alphabetically, new, most listed, most liked).
+          Total Songs: { getArtist.songs }, Liked songs: { getArtist.songLikes }
+          {allSongs}
+        </div>
+
+        <Comment/>
+        <CommentsList total={ getArtist.comments }/>
 
         <style jsx>{`
           .title, .description {
--- pages/artist.js
+++ pages/artist.js
@@ -1,80 +1,10 @@
-import Link from 'next/link'
 import { withRouter } from 'next/router'
-import Layout from '../components/layout'
 import WithData from '../lib/withData'
-import Comment from '../components/comment.comp'
-import CommentsList from '../components/commentsList.comp'
+import Layout from '../components/layout'
 import Artist from '../components/artist.comp'
-
-const recentlyAddedSongs = []
-for (let i = 0; i < 10; i++) {
-  recentlyAddedSongs.push(
-    <Link key={i} as="/song/1/slug" href={`/song?id=1`}>
-      <a>song title </a>
-    </Link>
-  )
-}
-
-const popularSongs = []
-for (let i = 0; i < 10; i++) {
-  popularSongs.push(
-    <Link key={i} as="/song/1/slug" href={`/song?id=1`}>
-      <a>song title </a>
-    </Link>
-  )
-}
-
-const allSongs = []
-for (let i = 0; i < 20; i++) {
-  allSongs.push(
-    <div key={i}>
-      <img src="https://via.placeholder.com/30?text=song+image"/>
-      <Link as="/song/1/slug" href={`/song?id=1`}>
-        <a>song title</a>
-      </Link>
-      <img src="https://via.placeholder.com/30?text=duration"/> 3:25
-      <img src="https://via.placeholder.com/30?text=playsCount"/> 2,323
-      <img src="https://via.placeholder.com/30?text=More+Actions"/>
-    </div>
-  )
-}
 
 export default withRouter(WithData((props) => (
   <Layout>
-    <Artist router={props.router} fixSlug={true} />
-    <div>
-      <img src="https://via.placeholder.com/80"/>Artist Name
-      <Link href=""><a>Like</a></Link> 2,233 liked them
-    </div>
-    <div>
-      New Songs
-      {recentlyAddedSongs}
-    </div>
-    <div>
-      Share
-      <Link href="/"><a>Facebook</a></Link>
-      <Link href="/"><a>Twitter</a></Link>
-      <Link href="/"><a>Google+</a></Link>
-      <input value="https://www.awtarika.com/artist/1/slug-here" readOnly/>
-    </div>
-    <div>
-      <img src="https://via.placeholder.com/728x90?text=728x90+Leaderboard+Ad+but+will+be+responsive"/>
-    </div>
-    <div>
-      Popular songs
-      {popularSongs}
-    </div>
-    <div>
-      Data?
-      3,444 listers
-      Songs played: 5,324,355
-    </div>
-    <div>
-      All songs (sort: alphabetically, new, most listed, most liked).
-      Total Songs: 3,344, Liked songs: 3,553
-      {allSongs}
-    </div>
-    <Comment/>
-    <CommentsList/>
+    <Artist router={ props.router } fixSlug={ true } />
   </Layout>
 )))
