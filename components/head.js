b07df8413ddae02c958857d4e10f5e9e99af76b7
Anas Qadrei <anasqadrei@gmail.com>
2020-01-10 10:45:06 +1100

    remove static folder in favor of public

--- components/head.js
+++ components/head.js
@@ -17,7 +17,7 @@ const Head = (props) => (
     <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
     <link rel="apple-touch-icon" href="/static/touch-icon.png" />
     <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
-    <link rel="icon" href="/static/favicon.ico" />
+    <link rel="icon" href="/favicon.ico" />
     <meta property="og:url" content={ props.asPath ? `${ ROOT_WEBSITE_URL }${ props.asPath }` : `${ ROOT_WEBSITE_URL }/` } />
     <meta property="og:title" content={ props.title ? `${ props.title } - ${ WEBSITE_TITLE }` : WEBSITE_TITLE } />
     <meta property="og:description" content={props.description || defaultDescription} />
