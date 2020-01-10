7d08c7e58b3500ff78b33bd19fbe789c416c5b57
Anas Qadrei <anasqadrei@gmail.com>
2020-01-10 11:26:28 +1100

    Custom Error Page

--- pages/_error.js
+++ pages/_error.js
@@ -1,25 +1,17 @@
-import React from 'react'
-
-export default class Error extends React.Component {
-  static getInitialProps({ res, err }) {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
-    return { statusCode }
-  }
-
-  render() {
-    return (
-      <p>
-        Design this page -
-        {this.props.statusCode
-          ? `XXX1 An error ${this.props.statusCode} occurred on server`
-          : 'XXX2 An error occurred on client'
-        }
-        <style jsx>{`
-          p {
-            text-align: center;
-          }
-        `}</style>
-      </p>
-    )
-  }
+function Error({ statusCode }) {
+  return (
+    <p>
+      Design this page -
+      {statusCode
+        ? `An error ${ statusCode } occurred on server`
+        : 'An error occurred on client'}
+    </p>
+  )
 }
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
+}
+
+export default Error
