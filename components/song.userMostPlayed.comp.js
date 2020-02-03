d8af3b3a51d01e04d0e89e1dc4e5cf15528a59e1
Anas Qadrei <anasqadrei@gmail.com>
2020-02-03 11:10:56 +1100

    user most played songs

---
+++ components/song.userMostPlayed.comp.js
@@ -0,0 +1,120 @@
+import { useQuery } from '@apollo/react-hooks'
+import { NetworkStatus } from 'apollo-client'
+import gql from 'graphql-tag'
+import * as Sentry from '@sentry/node'
+import SongItem from './song.item.comp'
+import ErrorMessage from './errorMessage'
+
+// TEMP: until we decide on the login mechanism
+const loggedOnUser = {
+  id: "1",
+  username: "Admin",
+}
+
+const SORT = '-plays'
+const PAGE_SIZE = 1
+const LIST_USER_PLAYED_SONGS_QUERY = gql`
+  query listUserPlayedSongs ($userId: ID!, $sort: String!, $page: Int!, $pageSize: Int!) {
+    listUserPlayedSongs(userId: $userId, sort: $sort, page: $page, pageSize: $pageSize) {
+      id
+      title
+      slug
+      artist {
+        id
+        name
+        slug
+      }
+      plays
+      duration
+      defaultImage {
+        url
+      }
+    }
+  }
+`
+
+// defaults
+let nextPage = true
+
+export default function UserRecentlyPlayedSongs() {
+  // set query variables
+  const queryVariables = {
+    userId: loggedOnUser.id,
+    sort: SORT,
+    page: 1,
+    pageSize: PAGE_SIZE,
+  }
+
+  // excute query
+  const { loading, error, data, fetchMore, networkStatus } = useQuery (
+    LIST_USER_PLAYED_SONGS_QUERY,
+    {
+      variables: queryVariables,
+      notifyOnNetworkStatusChange: true,
+    }
+  )
+
+  // loading more network status. fetchMore: query is currently in flight
+  const loadingMore = (networkStatus === NetworkStatus.fetchMore)
+
+  // get and append new fetched songs. also decide on paging
+  const loadMoreSongs = () => {
+    fetchMore({
+      variables: {
+        page: Math.ceil(listUserPlayedSongs.length/queryVariables.pageSize)+1
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        if (!fetchMoreResult || !fetchMoreResult.listUserPlayedSongs || (fetchMoreResult.listUserPlayedSongs && fetchMoreResult.listUserPlayedSongs.length === 0)) {
+          nextPage = false
+          return previousResult
+        }
+        return Object.assign({}, previousResult, {
+          listUserPlayedSongs: [...previousResult.listUserPlayedSongs, ...fetchMoreResult.listUserPlayedSongs],
+        })
+      },
+    })
+  }
+
+  // error handling
+  if (error) {
+    Sentry.captureException(error)
+    return <ErrorMessage/>
+  }
+
+  // initial loading
+  if (loading && !loadingMore) {
+    return (<div>Loading... (design this)</div>)
+  }
+
+  // get data
+  const { listUserPlayedSongs } = data
+
+  // in case no songs found
+  if (!listUserPlayedSongs || !listUserPlayedSongs.length) {
+    return (<div>no recently played songs found (design this)</div>)
+  }
+
+  // display songs
+  return (
+    <section>
+      My Recently Played
+      { listUserPlayedSongs && listUserPlayedSongs.map(song => (
+        <SongItem key={ song.id } song={ song } />
+      ))}
+
+      { (loadingMore || nextPage)?
+        <button onClick={ () => loadMoreSongs() } disabled={ loadingMore }>
+          { loadingMore ? 'Loading...' : 'Show More Songs المزيد' }
+        </button>
+        :
+        <p>all songs has been shown</p>
+      }
+
+      <style jsx>{`
+        .title, .description {
+          text-align: center;
+        }
+      `}</style>
+    </section>
+  )
+}
--- pages/most-played.js
+++ pages/most-played.js
@@ -1,26 +1,16 @@
-import Link from 'next/link'
+import WithData from '../lib/withData'
 import Layout from '../components/layout'
-import Head from '../components/head'
 import LibraryNav from '../components/libraryNav'
-import SongItem from '../components/songItem.comp'
+import UserMostPlayedSongs from '../components/song.userMostPlayed.comp'
 
-const songs = []
-for (let i = 0; i < 10; i++) {
-  songs.push(
-    <SongItem key={i}/>
-  )
-}
-
-export default (props) => (
+export default WithData(() => (
   <Layout>
     <LibraryNav/>
-    <img src="https://via.placeholder.com/80"/>Most Played
     <div>
-      <Link href=""><a>Play All</a></Link> | <Link href=""><a>Shuffle</a></Link>
+      <UserMostPlayedSongs/>
     </div>
-    {songs}
     <div>
       <img src="https://via.placeholder.com/728x90?text=728x90+Leaderboard+Ad+but+will+be+responsive"/>
     </div>
   </Layout>
-)
+))
