ae6cca92e267497f3f9d34e07066c6c4355460d4
Anas Qadrei <anasqadrei@gmail.com>
2020-05-21 14:06:05 +1000

    Customizing The 404 Page since 500 is customized (_error)

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Layout from 'components/layout'
+import Head from 'components/head'
+
+export default () => (
+  <Layout>
+    <Head title='Not Found'/>
+    404 Page not found. you can customise
+  </Layout>
+)
