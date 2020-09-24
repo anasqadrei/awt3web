d6ad83a86d12a6473c46f8d70756ad4101ce8277
Anas Qadrei <anasqadrei@gmail.com>
2020-09-24 10:56:56 +1000

    show navbar of authUser

--- components/nav.js
+++ components/nav.js
@@ -17,27 +17,12 @@ export default () => {
       <Link href="/song/upload">
         <a>Upload</a>
       </Link>
-      <Link href="/user/playlists-list">
-        <a>Library</a>
-      </Link>
-      <Link href="/user/playlists-list">
-        <a>My Playlists</a>
-      </Link>
-      <Link href="/user/recently-played">
-        <a>Recently Played</a>
-      </Link>
-      <Link href="/user/most-played">
-        <a>Most Played</a>
-      </Link>
-      <Link href="/user/liked">
-        <a>Liked</a>
-      </Link>
-      <Link href="/user/saved">
-        <a>Saved</a>
-      </Link>
       {
         getAuthUser ? (
           <div>
+            <Link href="/user/playlists-list">
+              <a>Library</a>
+            </Link>
             <Link href="/user/[id]/[slug]" as={ `/user/${ getAuthUser.id }/${ getAuthUser.slug }` }>
               <a>{ getAuthUser.username }</a>
             </Link>
