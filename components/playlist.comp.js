f15fa278c924d58f8c830a8e0233501b6098fc9f
Anas Qadrei <anasqadrei@gmail.com>
2020-10-01 11:51:02 +1000

    show private playlist only to its user

--- components/playlist.comp.js
+++ components/playlist.comp.js
@@ -86,70 +86,80 @@ export default () => {
   const { getPlaylist } = data
 
   // fix url in case it doesn't match the slug
-  validateUrl(router, 'playlist', getPlaylist.id, getPlaylist.slug)
+  !(getPlaylist.private && getAuthUser?.id !== getPlaylist.user.id) && validateUrl(router, 'playlist', getPlaylist.id, getPlaylist.slug)
 
   // display data
   return (
     <section>
-      <Head title={ getPlaylist.name }/>
+      {
+        !(getPlaylist.private && getAuthUser?.id !== getPlaylist.user.id) ? (
+          <div>
+            <Head title={ getPlaylist.name }/>
 
-      <div>
-        <img src={ getPlaylist.imageUrl ? getPlaylist.imageUrl : `https://via.placeholder.com/100?text=no+photo` }/>
-        <h1 className="title">{ getPlaylist.name }</h1>
-        <LikePlaylist playlistId={ getPlaylist.id }/>
-      </div>
-
-      <div>
-        <PlayPlaylist playlistId={ getPlaylist.id } shuffle={ false }/> | <PlayPlaylist playlistId={ getPlaylist.id } shuffle={ true }/> | 
-        <div hidden={ getAuthUser?.id !== getPlaylist.user.id }>
-          <button onClick={ () => { setUpdatePlaylistModalIsOpen(true) } }>Update Playlist</button> | 
-        </div>
-        <DeletePlaylist playlist={ getPlaylist }/>
-      </div>
-
-      <Modal isOpen={ updatePlaylistModalIsOpen } onRequestClose={ () => { setUpdatePlaylistModalIsOpen(false) } } style={ modalStyles } contentLabel="update playlist modal">
-        <button onClick={ () => { setUpdatePlaylistModalIsOpen(false) } }>
-          Close
-        </button>
-        <h2>Update Playlist</h2>
-        <UpdatePlaylist playlist={ getPlaylist }/>
-      </Modal>
-
-      <SharePlaylist playlistId={ getPlaylist.id }/>
-
-      <div>
-        <img src="https://via.placeholder.com/728x90?text=728x90+Leaderboard+Ad+but+will+be+responsive"/>
-      </div>
-
-      <div>
-        Data:
-        { getPlaylist.desc && <div dangerouslySetInnerHTML={{ __html: getPlaylist.desc.replace(/#[^\s<]+/g,'') }}/> }
-        {
-          getPlaylist.hashtags && getPlaylist.hashtags.map(hashtag => (
-            <div key={ hashtag }>
-              <Link href="/hashtag/[hashtag]" as={ `/hashtag/${ hashtag }` }><a>#{ hashtag }</a></Link>
+            <div>
+              <img src={ getPlaylist.imageUrl ? getPlaylist.imageUrl : `https://via.placeholder.com/100?text=no+photo` }/>
+              <h1 className="title">{ getPlaylist.name }</h1>
+              <LikePlaylist playlistId={ getPlaylist.id }/>
             </div>
-          ))
-        }
-        { getPlaylist.usersPlayed ? `${ getPlaylist.usersPlayed } listeners` : null }
-        Played: { getPlaylist.plays ? getPlaylist.plays : 0 }
-        <br/>
-        private: { getPlaylist.private.toString() }
-        <br/>
-        Added by: <Link href="/user/[id]/[slug]" as={ `/user/${ getPlaylist.user.id }/${ getPlaylist.user.slug }` }><a>{ getPlaylist.user.username }</a></Link> on { getPlaylist.createdDate }
-      </div>
 
-      <div>
-        { getPlaylist.songs && getPlaylist.songs.map((song, index) => (
-          <div key={ `${ index }-${ song.id }` }>
-            <SongItem song={ song }/>
-            <RemoveSongFromPlaylist playlist={ getPlaylist } song={ song } index={ index }/>
+            <div>
+              <PlayPlaylist playlistId={ getPlaylist.id } shuffle={ false }/> | <PlayPlaylist playlistId={ getPlaylist.id } shuffle={ true }/> | 
+              <div hidden={ getAuthUser?.id !== getPlaylist.user.id }>
+                <button onClick={ () => { setUpdatePlaylistModalIsOpen(true) } }>Update Playlist</button> | 
+              </div>
+              <DeletePlaylist playlist={ getPlaylist }/>
+            </div>
+
+            <Modal isOpen={ updatePlaylistModalIsOpen } onRequestClose={ () => { setUpdatePlaylistModalIsOpen(false) } } style={ modalStyles } contentLabel="update playlist modal">
+              <button onClick={ () => { setUpdatePlaylistModalIsOpen(false) } }>
+                Close
+              </button>
+              <h2>Update Playlist</h2>
+              <UpdatePlaylist playlist={ getPlaylist }/>
+            </Modal>
+
+            <SharePlaylist playlistId={ getPlaylist.id }/>
+
+            <div>
+              <img src="https://via.placeholder.com/728x90?text=728x90+Leaderboard+Ad+but+will+be+responsive"/>
+            </div>
+
+            <div>
+              Data:
+              { getPlaylist.desc && <div dangerouslySetInnerHTML={{ __html: getPlaylist.desc.replace(/#[^\s<]+/g,'') }}/> }
+              {
+                getPlaylist.hashtags && getPlaylist.hashtags.map(hashtag => (
+                  <div key={ hashtag }>
+                    <Link href="/hashtag/[hashtag]" as={ `/hashtag/${ hashtag }` }><a>#{ hashtag }</a></Link>
+                  </div>
+                ))
+              }
+              { getPlaylist.usersPlayed ? `${ getPlaylist.usersPlayed } listeners` : null }
+              Played: { getPlaylist.plays ? getPlaylist.plays : 0 }
+              <br/>
+              private: { getPlaylist.private.toString() }
+              <br/>
+              Added by: <Link href="/user/[id]/[slug]" as={ `/user/${ getPlaylist.user.id }/${ getPlaylist.user.slug }` }><a>{ getPlaylist.user.username }</a></Link> on { getPlaylist.createdDate }
+            </div>
+
+            <div>
+              { getPlaylist.songs && getPlaylist.songs.map((song, index) => (
+                <div key={ `${ index }-${ song.id }` }>
+                  <SongItem song={ song }/>
+                  <RemoveSongFromPlaylist playlist={ getPlaylist } song={ song } index={ index }/>
+                </div>
+              )) }
+            </div>
+
+            { !getPlaylist.private && <CreateComment collection={ PLAYLISTS_COLLECTION } id={ getPlaylist.id }/> }
+            <CommentsList collection={ PLAYLISTS_COLLECTION } id={ getPlaylist.id }/>
           </div>
-        )) }
-      </div>
-
-      { !getPlaylist.private && <CreateComment collection={ PLAYLISTS_COLLECTION } id={ getPlaylist.id }/> }
-      <CommentsList collection={ PLAYLISTS_COLLECTION } id={ getPlaylist.id }/>
+        ) : (
+          <div>
+            هذي القائمة مغلقة
+          </div>
+        )
+      }
 
       <style jsx>{`
         .title, .description {
