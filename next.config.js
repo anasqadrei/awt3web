be4d8d28bdd1c971358ebfe950e4f7b2bb45b03a
Anas Qadrei <anasqadrei@gmail.com>
2020-10-06 11:33:01 +1100

    redirects from old awtphase2

--- next.config.js
+++ next.config.js
@@ -2,6 +2,35 @@ const withSourceMaps = require('@zeit/next-source-maps')
 
 module.exports = withSourceMaps({
   target: 'serverless',
+  async redirects() {
+    return [
+      {
+        source: '/artists-list',
+        destination: '/browse',
+        permanent: true, 
+      },
+      { 
+        source: '/about-us', 
+        destination: '/about', 
+        permanent: true,
+      },
+      { 
+        source: '/terms', 
+        destination: '/legal/terms', 
+        permanent: true,
+      },
+      { 
+        source: '/privacy', 
+        destination: '/legal/privacy', 
+        permanent: true,
+      },
+      { 
+        source: '/copyright', 
+        destination: '/legal/copyright', 
+        permanent: true,
+      },
+    ]
+  },
   webpack: (config, options) => {
     // Asking Webpack to replace @sentry/node imports with @sentry/browser when
     // building the browser's bundle
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import Layout from 'components/layout'
 import Head from 'components/head'
 import NewSongs from 'components/song.new.comp'
@@ -9,52 +10,62 @@ import TopHashtagsInSongs from 'components/hashtag.topInSongs.comp'
 import TopHashtagsInPlaylists from 'components/hashtag.topInPlaylists.comp'
 import TopSearchTerms from 'components/search.topTerms.comp'
 
-const Page = () => (
-  <Layout>
-    <Head/>
-    <div>
-      <h1>Awtarika</h1>
-      <p>اهلا بكم في موقع اوتاريكا للاغاني حيث نتمنى ان تسعدوا بقضاء افضل الاوقات بسماع كل ما يحلوا لكم من اغاني و موسيقى.</p>
-      <NewSongs/>
+const Page = () => {
+
+  // remove old awtphase2 hashbangs /#!/
+  const router = useRouter()
+  if (router.asPath.match(/^\/\#\!\//)) {
+    router.push(router.asPath.substring(3))
+    return null
+  }
+
+  return (
+    <Layout>
+      <Head/>
       <div>
-        <img src="https://via.placeholder.com/728x90?text=728x90+Leaderboard+Ad+but+will+be+responsive"/>
-      </div>
-      <div>
-        <TopSongs/>
-      </div>
-      <div>
-        Trending Searches
-        <TopSearchTerms/>
-      </div>
-      <div>
-        <img src="https://via.placeholder.com/728x90?text=728x90+Leaderboard+Ad+but+will+be+responsive"/>
-      </div>
-      <div>
-        <UserRecentlyPlayedSongs snippet={ true }/>
-        <Link href="/user/recently-played">
-          <a>More</a>
+        <h1>Awtarika</h1>
+        <p>اهلا بكم في موقع اوتاريكا للاغاني حيث نتمنى ان تسعدوا بقضاء افضل الاوقات بسماع كل ما يحلوا لكم من اغاني و موسيقى.</p>
+        <NewSongs/>
+        <div>
+          <img src="https://via.placeholder.com/728x90?text=728x90+Leaderboard+Ad+but+will+be+responsive"/>
+        </div>
+        <div>
+          <TopSongs/>
+        </div>
+        <div>
+          Trending Searches
+          <TopSearchTerms/>
+        </div>
+        <div>
+          <img src="https://via.placeholder.com/728x90?text=728x90+Leaderboard+Ad+but+will+be+responsive"/>
+        </div>
+        <div>
+          <UserRecentlyPlayedSongs snippet={ true }/>
+          <Link href="/user/recently-played">
+            <a>More</a>
+          </Link>
+        </div>
+        <div>
+          <img src="https://via.placeholder.com/728x90?text=728x90+Leaderboard+Ad+but+will+be+responsive"/>
+        </div>
+        <div>
+          song hashtags
+          <TopHashtagsInSongs/>
+        </div>
+        <div>
+          playlist hashtags
+          <TopHashtagsInPlaylists/>
+        </div>
+        <div>
+          popular artists
+          <TopLikedArtists/>
+        </div>
+        <Link href="browse">
+          <a>Browse More</a>
         </Link>
       </div>
-      <div>
-        <img src="https://via.placeholder.com/728x90?text=728x90+Leaderboard+Ad+but+will+be+responsive"/>
-      </div>
-      <div>
-        song hashtags
-        <TopHashtagsInSongs/>
-      </div>
-      <div>
-        playlist hashtags
-        <TopHashtagsInPlaylists/>
-      </div>
-      <div>
-        popular artists
-        <TopLikedArtists/>
-      </div>
-      <Link href="browse">
-        <a>Browse More</a>
-      </Link>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default Page
