a19734cb422c69fbe2dae4afb84097b935e788a7
Anas Qadrei <anasqadrei@gmail.com>
2020-11-27 13:51:39 +1100

    getUploadSignedURL - part 2. createSongImage

--- components/songImage.create.comp.js
+++ components/songImage.create.comp.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { gql, useApolloClient, useMutation } from '@apollo/client'
 import * as Sentry from '@sentry/node'
 import { queryAuthUser } from 'lib/localState'
@@ -6,19 +7,23 @@ import ErrorMessage from 'components/errorMessage'
 
 const FORM_FILE = "file"
 const CREATE_SONG_IMAGE_MUTATION = gql`
-  mutation createSongImage ($songId: ID!, $userId: ID!) {
-    createSongImage(songId: $songId, userId: $userId) {
+  mutation createSongImage ($file: String!, $songId: ID!, $userId: ID!) {
+    createSongImage(file: $file, songId: $songId, userId: $userId) {
       id
     }
   }
 `
 
 const Comp = (props) => {
+  // upload state variables
+  const [uploadProgress, setUploadProgress] = useState(0)
+  const [errorUpload, setErrorUpload] = useState(false)
+
   // apollo client for query later
   const apolloClient = useApolloClient()
 
   // mutation tuple
-  const [createSongImage, { loading, error, data }] = useMutation(
+  const [createSongImage, { loading, error: errorCreate, data }] = useMutation(
     CREATE_SONG_IMAGE_MUTATION,
     {
       onError: (error) => {
@@ -44,57 +49,55 @@ const Comp = (props) => {
       fetchPolicy: 'no-cache',
       query: GET_UPLOAD_SIGNED_URL_QUERY,
     })
-    
+  
     // using XMLHttpRequest rather than fetch() for its onprogress event
     const xhr = new XMLHttpRequest()
 
     // xhr event: track upload progress
     xhr.upload.onprogress = function(event) {
-      console.log(`Uploaded ${ event.loaded } of ${ event.total } (${ Math.trunc((event.loaded/event.total)*100) }%)`);
+      setUploadProgress(Math.trunc((event.loaded / event.total) * 100))
     }
 
     // xhr event: track completion: both successful or not
     xhr.onloadend = function() {
-      console.log(xhr);
       if (xhr.status == 200) {
-        console.log("success");
-        // console.log(data.getUploadSignedURL.match(/(?<=(\.com\/))(.+)(?=\?)/)[0]);
+        // execute mutation
+        // refetch getSong because updating list of images in cache is a hassle
+        createSongImage({
+          variables: {
+            file: data.getUploadSignedURL.match(/(?:\.com\/)(.+)(?=\?)/)[1],
+            songId: props.songId,
+            userId: getAuthUser?.id,
+          },
+          refetchQueries: () => [{
+            query: GET_SONG_QUERY,
+            variables: { id: props.songId },
+          }],
+          awaitRefetchQueries: true,
+        })
       } else {
-        console.log("error " + this.status);
+        // report file wasn't uploaded
+        setErrorUpload(true)
+        Sentry.captureMessage(xhr.responseText)
       }
     }
 
     // upload file
     xhr.open(`PUT`, data?.getUploadSignedURL)
     xhr.send(file)
-
-    // // execute mutation
-    // // refetch getSong because updating list of images in cache is a hassle
-    // createSongImage({
-    //   variables: {
-    //     // TODO: file
-    //     // file: file,
-    //     songId: props.songId,
-    //     userId: getAuthUser?.id,
-    //   },
-    //   refetchQueries: () => [{
-    //     query: GET_SONG_QUERY,
-    //     variables: { id: props.songId },
-    //   }],
-    //   awaitRefetchQueries: true,
-    // })
   }
 
   // display component
   return (
     <form onSubmit={ handleSubmit }>
-      <input name={ FORM_FILE } type="file" accept="image/png, image/jpeg" required/>
+      <input name={ FORM_FILE } type="file" accept="image/jpeg" required/>
       <button type="submit" disabled={ loading || data?.createSongImage }>
         Add Song Image
       </button>
 
+      { uploadProgress > 0 && <div>uploading { uploadProgress }%</div> }
       { loading && <div>mutating (design this)</div> }
-      { error && <ErrorMessage/> }
+      { (errorUpload || errorCreate) && <ErrorMessage/> }
       { data?.createSongImage && <div>song-image added. Check later(won't show instantly)</div> }
     </form>
   )
